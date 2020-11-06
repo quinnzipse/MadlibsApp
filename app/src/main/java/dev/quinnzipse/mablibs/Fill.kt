a8a857119730466f562517f1dd6d044e1bc850d2
Quinn Zipse <qzipse@outlook.com>
2020-11-06 14:02:04 -0600

    Working on my file reading issues

--- app/src/main/java/dev/quinnzipse/mablibs/Fill.kt
+++ app/src/main/java/dev/quinnzipse/mablibs/Fill.kt
@@ -4,17 +4,31 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_fill.*
+import java.io.BufferedReader
+import java.io.File
+import java.util.*
 
 class Fill : AppCompatActivity() {
-    private var wordsLeft: Int = 7
-    private val words: Int = wordsLeft - 1
+
+    private var wordsLeft: Int = -1
+    private var words: Int = -1
     private lateinit var list: Array<String>
 
+    private val inputStream = File("madlib1.txt").inputStream()
+    private val inString : String = inputStream.bufferedReader().use { it.readText() }
+    private val regex : Regex = Regex("<[A-Za-z]*>")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fill)
+
+        val matches = regex.findAll(inString)
+
+        wordsLeft = matches.count()
+        words = wordsLeft - 1
+
         words_left.text = String.format(getString(R.string.fill_left), wordsLeft)
         nextButton.setOnClickListener { onNextButton() }
+
         list = Array(wordsLeft) { "" }
     }
 
